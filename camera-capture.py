9d285f279b9b4805779f63dab97030161d26201e
Tom Potter <tom.potter@bbc.co.uk>
2024-04-03 13:04:22 +0100

    fleshed out audio recording into overlapping thread-safe buffers

--- camera-capture.py
+++ camera-capture.py
@@ -1,28 +1,34 @@
-import cv2
-import math
-import wave
-import time
-import pyaudio
 import datetime
-import numpy as np
-from threading import Thread
+import math
+import time
+import signal
+import sys
 from collections import deque
+from threading import Thread
+import matplotlib.pyplot as plt
+
+import cv2
+import numpy as np
+import pyaudio
+import sounddevice
 
 
 class VideoStream():
-    def __init__(self, camera=0):
+    def __init__(self, device=0):
         # Define a video capture object
-        self.video_stream = cv2.VideoCapture(camera)
+        self.video_stream = cv2.VideoCapture(device)
         self.frame_rate = self.video_stream.get(cv2.CAP_PROP_FPS)
+        self.stream_open = False
 
-    def launch(self, frame_queue=None, display_stream=False):
+    def launch(self, frame_queue, display_stream=False):
+        self.stream_open = True
         # Show the video stream (no processing)
         if display_stream:
             while self.video_stream.isOpened():
                 _, frame = self.video_stream.read()
                 timestamp = datetime.datetime.now().strftime('%H:%M:%S.%f')
                 cv2.imshow('Video Stream', frame)
-                print(f"Video timestamp counter: {timestamp}", end='\r')
+                # print(f"Timestamp counter: {timestamp}", end='\r')
 
                 # esc to quit
                 if cv2.waitKey(1) == 27:
@@ -35,95 +41,105 @@ class VideoStream():
             self.video_stream.read()
             self.video_stream.read()
 
-            while self.video_stream.isOpened():
+            while self.stream_open:
                 # Capture the video frame by frame
                 _, frame = self.video_stream.read()
                 timestamp = datetime.datetime.now().strftime('%H:%M:%S.%f')
                 frame_queue.append((timestamp, frame))
-                print(f"Video timestamp counter: {timestamp}", end='\r')
+                print(f"Timestamp counter: {timestamp}", end='\r')
+
+        self.video_stream.release()
+        cv2.destroyAllWindows()
+        print("\nVideo thread ended.")
 
     def kill(self):
-        # After the loop release the cap object
-        self.video_stream.release()
-
-        # Destroy all the windows
-        cv2.destroyAllWindows()
-
-        print("\nStream open: ", self.video_stream.isOpened())
+        self.stream_open = False
 
 
 class AudioStream():
-    def __init__(self):
+    def __init__(self, device=1, sample_rate=44100):
         self.format = pyaudio.paInt16
-        self.rate = 44100
+        self.rate = sample_rate
         self.chunk = 1024
-        self.record_time_secs = 6
-        self.output = "recording.wav"
+        self.stream = None
+        self.stream_open = False
 
-        self.audio_device = 1
+        self.audio_device = device
         self.audio = pyaudio.PyAudio()
         self.audio_channels = self.audio.get_device_info_by_host_api_device_index(0, self.audio_device).get('maxInputChannels')
 
-    def launch(self):
-        "Audio starts being recorded"
-        print(f'     * Audio input device:', self.audio_device)
-        print(f'     * Audio input channels:', self.audio_channels, end='\n\n')
+        print(f'     * Audio input device         :', self.audio_device)
+        print(f'     * Audio input channels       :', self.audio_channels)
 
-        # start Recording
+    def launch(self, frame_queue):
+        # Start audio recording
+        self.stream_open = True
         stream = self.audio.open(
             format=self.format, rate=self.rate, input=True,
             input_device_index=self.audio_device, channels=self.audio_channels,
             frames_per_buffer=self.chunk
         )
 
-        print("recording...")
-        frames = []
+        while self.stream_open:
+            frame = stream.read(self.chunk)
+            timestamp = datetime.datetime.now().strftime('%H:%M:%S.%f')
+            frame_queue.append((timestamp, frame))
+            print(f"Timestamp counter: {timestamp}", end='\r')
+            # print(f"Audio frame counter: {len(frame_queue) * self.chunk}", end='\r')
 
-        for _ in range(0, int(self.rate / self.chunk * self.record_time_secs)):
-            data = stream.read(self.chunk)
-            frames.append(data)
-
-        print("finished recording")
-
-        # stop Recording
         stream.stop_stream()
         stream.close()
         self.audio.terminate()
+        print("Audio thread ended.")
 
-        waveFile = wave.open(self.output, 'wb')
-        waveFile.setnchannels(self.audio_device)
-        waveFile.setsampwidth(self.audio.get_sample_size(self.format))
-        waveFile.setframerate(self.rate)
-        waveFile.writeframes(b''.join(frames))
-        waveFile.close()
+    def kill(self):
+        self.stream_open = False
 
 
-def video_processing(video_frames, frames_per_second, buffer_length_secs=10, overlap_length_secs=2):
+def av_processing(audio_module, video_module,
+                  audio_frames, video_frames, video_fps,
+                  audio_buffer_len_f=44100, audio_overlap_len_f=4410,
+                  video_buffer_len_s=10, video_overlap_len_s=2):
+
     # Processing frame buffer parameters
-    buffer_length_frames = math.ceil(frames_per_second * buffer_length_secs)
-    overlap_length_frames = math.ceil(frames_per_second * overlap_length_secs)
-    segment_index = 0
+    video_buffer_len_f = math.ceil(video_fps * video_buffer_len_s)
+    video_overlap_len_f = math.ceil(video_fps * video_overlap_len_s)
 
-    print(f"     * Video segments size        : {buffer_length_frames}")
-    print(f"     * Overlapping frames         : {overlap_length_frames}", end='\n\n')
+    print(f"     * Audio segment size         : {audio_buffer_len_f}")
+    print(f"     * Audio overlap size         : {audio_overlap_len_f}")
+    print(f"     * Video frame rate           : {video_fps}")
+    print(f"     * Video segment size         : {video_buffer_len_f}")
+    print(f"     * Video overlap size         : {video_overlap_len_f}", end='\n\n')
+
+    audio_segment_index = 0
+    video_segment_index = 0
 
     while True:
-        try:
-            if len(video_frames) > buffer_length_frames:
-                video_segment = collate_frames(video_frames, buffer_length_frames, overlap_length_frames, segment_index)
-                detection(video_segment, segment_index)
-                segment_index += 1
+        # Audio processing module
+        if len(audio_frames) >= audio_buffer_len_f:
+            audio_segment = collate_frames(
+                audio_frames, audio_buffer_len_f, audio_overlap_len_f,
+                index=audio_segment_index, av_type='audio')
 
-        except KeyboardInterrupt:
-            video.kill()
-            audio_thread.join()
-            video_thread.join()
-            processing_thread.join()
+            audio_detection(audio_segment, audio_segment_index)
+            audio_segment_index += 1
+
+        # Video processing module
+        if len(video_frames) >= video_buffer_len_f:
+            video_segment = collate_frames(
+                video_frames, video_buffer_len_f, video_overlap_len_f,
+                index=video_segment_index, av_type='video')
+
+            video_detection(video_segment, video_segment_index)
+            video_segment_index += 1
+
+        if not audio_module.stream_open and not video_module.stream_open and len(audio_frames) < audio_buffer_len_f and len(video_frames) < video_buffer_len_f:
+            print(f"\nExiting processing module. {len(audio_frames)} audio and {len(video_frames)} video frames remain unprocessed.")
             break
 
 
-def collate_frames(frame_queue, frames_per_buffer, overlap_frames, index=0):
-    print(f"\n\n * New video segment ({index}):")
+def collate_frames(frame_queue, frames_per_buffer, overlap_frames, index=0, av_type='audio'):
+    print(f"\n\n * New {av_type} segment ({index}):")
     print(f"     * Input queue length  : {len(frame_queue)}")
 
     # Add main frames in video segment to buffer
@@ -136,17 +152,28 @@ def collate_frames(frame_queue, frames_per_buffer, overlap_frames, index=0):
 
     print(f"     * Segment frame count : {len(frame_buffer)}")
     print(f"     * Segment time range  : {frame_buffer[0][0]} => {frame_buffer[-1][0]}")
-    print(f"     * self.output queue length : {len(frame_queue)}", end='\n\n')
+    print(f"     * Output queue length : {len(frame_queue)}", end='\n\n')
 
     return frame_buffer
 
 
-def detection(video, index=0):
+def audio_detection(audio_content, index=0):
+    # fig = plt.figure()
+    # s = fig.add_subplot(111)
+    amplitude = np.frombuffer(b''.join([x[1] for x in audio_content]), np.int16)
+    # s.plot(amplitude)
+    # fig.savefig('audio.png')
+
+    print(f"\n\n * Audio detection ({index}):")
+    print(f"     * Audio amplitudes    : {amplitude}")
+
+
+def video_detection(video_content, index=0):
     # Detect average brightness
     brightness = []
     black_frame_detected = False
 
-    for time, frame in video:
+    for time, frame in video_content:
         average_brightness = np.average(np.linalg.norm(frame, axis=2)) / np.sqrt(3)
         brightness.append(average_brightness)
         if average_brightness < 10: black_frame_detected = True
@@ -156,32 +183,44 @@ def detection(video, index=0):
     print(f"     * Black frame detected: {black_frame_detected}", end='')
 
 
+def signal_handler(sig, frame):
+    print("\nCtrl+C detected. Stopping all AV threads.")
+    audio.kill()
+    video.kill()
+
+
 if __name__ == '__main__':
     # Switch between setup video stream & processing stream
-    camera_device = 0
-    processing_module = True
-    audio = AudioStream()
-    video = VideoStream(camera=camera_device)
-    video_fps = video.frame_rate
+    setup_mode_only = False
+    audio_device = 1
+    video_device = 0
 
+    # Set up the signal handler for Ctrl+C
+    signal.signal(signal.SIGINT, signal_handler)
+
+    print("\nAudio devices available: \n", sounddevice.query_devices())
     print(f"\n * Parameters:")
-    print(f"     * Process video (no display) : {processing_module}")
-    print(f"     * Camera capture source      : {camera_device}")
-    print(f"     * Capture frame self.rate         : {video_fps}")
+    print(f"     * Setup mode (no processing) : {setup_mode_only}")
+    print(f"     * Video capture source       : {video_device}")
 
-    if not processing_module:
+    # Set up audio and video streams
+    audio = AudioStream(device=audio_device)
+    video = VideoStream(device=video_device)
+
+    if setup_mode_only:
         # Set up stream to show content but do no processing
         print()
         video.launch(display_stream=True)
     else:
         # Initialise video stream
+        audio_frame_queue = deque()
         video_frame_queue = deque()
 
         # Set up and launch audio-video stream thread and processing thread
-        audio_thread = Thread(target=audio.launch, args=())
+        audio_thread = Thread(target=audio.launch, args=(audio_frame_queue,))
         video_thread = Thread(target=video.launch, args=(video_frame_queue,))
-        processing_thread = Thread(target=video_processing, args=(video_frame_queue, video_fps,))
 
         audio_thread.start()
         video_thread.start()
-        processing_thread.start()
+
+        av_processing(audio, video, audio_frame_queue, video_frame_queue, video.frame_rate)
