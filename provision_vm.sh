4f1fe5789627e9f66dbcdeabdd90896a6b6641e7
Tom Potter <tom.potter@bbc.co.uk>
2024-04-15 11:48:30 +0100

    successfully read audio and video files from s3

--- provision_vm.sh
+++ provision_vm.sh
@@ -2,6 +2,10 @@
 apt-get install python3-venv
 python3 -m venv venv
 source venv/bin/activate
+
 pip3 install boto3
 pip3 install awscli
 pip3 install numpy
+
+apt-get update && apt-get install ffmpeg libsm6 libxext6  -y
+pip3 install opencv-python
